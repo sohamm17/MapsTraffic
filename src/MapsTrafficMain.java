d75c3cf8f14749fc92ddfc60c92cdcc51132462b
soham <soham.m17@gmail.com>
2015-03-25 01:12:54 -0600

    Speed considering traffic from HereMaps API

--- src/MapsTrafficMain.java
+++ src/MapsTrafficMain.java
@@ -29,7 +29,7 @@ import javax.json.stream.JsonParser;
 
 public class MapsTrafficMain {
 
-	static final String PUBLIC_API_KEY = "AIzaSyBNJndqJCWHbRrZ4DbGzWEQ19ROJYfM8wg";
+	static final String Google_PUBLIC_API_KEY = "AIzaSyBNJndqJCWHbRrZ4DbGzWEQ19ROJYfM8wg";
 	static Random rand;
 
 	public static void main(String[] args) throws Exception {
@@ -37,8 +37,16 @@ public class MapsTrafficMain {
 		MapsTrafficMain myMap = new MapsTrafficMain();
 		//myMap.hereMapsAPI();
 		
+		Traffic realTimeTraffic = new Traffic();
+		
+		String[] crossingRoads = {"82 Ave", "78 Ave"};
+		System.out.println(
+				realTimeTraffic.getAverageSpeed(53.5145554, -113.5103963, 53.5197864, -113.5212539, "109 St", "+", crossingRoads)
+				);
+		
+		/*
 		rand = new Random(50);// giving seed value to get finite result everytime
-		GeoApiContext context = new GeoApiContext().setApiKey(PUBLIC_API_KEY);
+		GeoApiContext context = new GeoApiContext().setApiKey(Google_PUBLIC_API_KEY);
 		
 		DirectionsRoute[] routes = DirectionsApi.newRequest(context)
 		        .origin("7708 109 Street Edmonton NW, AB, Canada")
@@ -64,85 +72,7 @@ public class MapsTrafficMain {
 //					+ " - " + eachStep.distance.inMeters + "m\n");
 		}
 		System.out.println("Starting time: " + routes[0].legs[0].arrivalTime.toLocalDateTime());
-	}
-	
-	public void hereMapsAPI() throws URISyntaxException
-	{
-		try (CloseableHttpClient httpClient = HttpClientBuilder.create().build())
-		{
-			String url = "http://traffic.cit.api.here.com/traffic/6.1/flow.json";
-			
-			URIBuilder builder = new URIBuilder(url)
-				.addParameter("app_id", "DemoAppId01082013GAL")
-				.addParameter("app_code", "AJKnXv84fjrb0KIHawS0Tg")
-				.addParameter("bbox", "53.5145554,-113.5103963;53.5197864,-113.5212539");
-			
-            HttpGet request = new HttpGet(builder.build());
-            
-            
-            request.addHeader("content-type", "application/json");
-            HttpResponse result = httpClient.execute(request);
-
-            String json = EntityUtils.toString(result.getEntity(), "UTF-8");
-            System.out.println(json);
-            try
-            {
-            	StringReader reader = new StringReader(json);
-                JsonReader jsonReader = Json.createReader(reader);
-                JsonObject wholeObject = jsonReader.readObject();
-                
-                JsonArray RoadWays = (JsonArray) wholeObject.get("RWS");
-                
-                for(JsonValue eachRWS : RoadWays)
-                {
-                	JsonObject eachRWSObject = (JsonObject) eachRWS;
-                	JsonArray Roads = eachRWSObject.getJsonArray("RW");
-                	
-                	for(JsonValue eachRoad : Roads)
-                	{
-                		JsonObject eachRoadObject = (JsonObject) eachRoad;
-                		
-                		System.out.println(eachRoadObject.getString("DE"));
-                		
-                		JsonArray flowItemElements = eachRoadObject.getJsonArray("FIS");
-                		
-                		for(JsonValue eachFlowItemElement : flowItemElements)
-                		{
-                			JsonArray singleFlowItems = ((JsonObject)eachFlowItemElement).getJsonArray("FI");
-                			
-                			for(JsonValue eachSingleFlowItem: singleFlowItems)
-                			{
-                				JsonObject singleFlowTMC = ((JsonObject)eachSingleFlowItem).getJsonObject("TMC");
-                				System.out.print(singleFlowTMC.getString("DE") + "-" + singleFlowTMC.getJsonNumber("LE") + " miles");
-                				
-                				JsonArray currentFlows = ((JsonObject)eachSingleFlowItem).getJsonArray("CF");
-                				
-                				for(JsonValue eachCurrentFlow : currentFlows)
-                				{
-                					JsonObject eachCurrentFlowObject = ((JsonObject)eachCurrentFlow);
-                					System.out.println(" --> FreeFlow: " + eachCurrentFlowObject.getJsonNumber("FF")
-                							+ ", Speed(by limit): " + eachCurrentFlowObject.getJsonNumber("SP")
-                							+ ", Speed(not limit): " + eachCurrentFlowObject.getJsonNumber("SU")
-                							+ ", Jam Factor: " + eachCurrentFlowObject.getJsonNumber("JF")
-                							+ ", Confidence: " + eachCurrentFlowObject.getJsonNumber("CN")
-                							);
-                				}
-                			}
-                		}
-                		System.out.println();
-                	}
-                }
-            } 
-            catch (Exception e)
-            {
-            	System.err.println("Error: " + e.getMessage());
-            	e.printStackTrace();
-            }
-
-        }
-		catch (IOException ex) 
-        {
-        }
+		*/
 	}
 	
 	//adding delay to particular step.
